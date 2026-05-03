2a25745a47bab5bc770cbc7c1d4c596ae2155f69
ruben <ruben-arcruni@yandex.ru>
2026-05-03 23:20:23 +0300

    feature:added streaming of stat

--- admin.go
+++ admin.go
@@ -27,6 +27,8 @@ func NewAdminManager() *AdminManager {
 func (am *AdminManager) Publish(evt *Event) {
 	am.mu.Lock()
 	defer am.mu.Unlock()
+	am.methodCounts[evt.Method]++
+    am.consumerCounts[evt.Consumer]++
 	for ch := range am.subsribers {
 		select {
 		case ch <- evt:
@@ -61,22 +63,56 @@ func (am *AdminManager) Logging(n *Nothing, stream grpc.ServerStreamingServer[Ev
 }
 
 func (am *AdminManager) Statistics(n *StatInterval, stream grpc.ServerStreamingServer[Stat]) error {
-	ticker:=time.NewTicker(time.Duration(n.IntervalSeconds)*time.Second)
-	defer ticker.Stop()
-	stat:=&Stat{
-		ByMethod: am.methodCounts,
-		ByConsumer: am.consumerCounts,
-	}
-	for {
-		select {
-		case <-ticker.C:
-			err:=stream.Send(stat)
-			if err != nil {
-				return err
-			}
-		case <-stream.Context().Done():
-			return nil
-		}
+    // Capture initial counters (before any events for this stream are processed)
+    am.mu.Lock()
+    baseMethod := make(map[string]uint64)
+    for k, v := range am.methodCounts {
+        baseMethod[k] = v
+    }
+    baseConsumer := make(map[string]uint64)
+    for k, v := range am.consumerCounts {
+        baseConsumer[k] = v
+    }
+    am.mu.Unlock()
 
-	}
+    ticker := time.NewTicker(time.Duration(n.IntervalSeconds) * time.Second)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ticker.C:
+            am.mu.Lock()
+            currentStat := &Stat{
+                Timestamp:  time.Now().Unix(),
+                ByMethod:   make(map[string]uint64),
+                ByConsumer: make(map[string]uint64),
+            }
+            for k, v := range am.methodCounts {
+                if v > baseMethod[k] {
+                    currentStat.ByMethod[k] = v - baseMethod[k]
+                }
+            }
+            for k, v := range am.consumerCounts {
+                if v > baseConsumer[k] {
+                    currentStat.ByConsumer[k] = v - baseConsumer[k]
+                }
+            }
+            // Update base for next interval
+            baseMethod = make(map[string]uint64)
+            for k, v := range am.methodCounts {
+                baseMethod[k] = v
+            }
+            baseConsumer = make(map[string]uint64)
+            for k, v := range am.consumerCounts {
+                baseConsumer[k] = v
+            }
+            am.mu.Unlock()
+
+            if err := stream.Send(currentStat); err != nil {
+                return err
+            }
+        case <-stream.Context().Done():
+            return nil
+        }
+    }
 }
